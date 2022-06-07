5258d0e46b7228f6a0590ec356c8e5a3cafd585b
chris.g.morton@elmutility.com <chris.g.morton@elmutility.com>
2022-06-07 17:24:14 -0600

    axios and cors added and functional

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const productsArray = [
@@ -18,8 +19,20 @@ function App() {
   const [products, setProducts] = useState(productsArray);
 
   const addProduct = () => {
-    setProducts([...products, {name: "Product4", price: 34} ])
+    //TODO: Lookie at prevState which takes in the Previous state of hook.
+    setProducts(prevState => [...prevState, {name: `Product ${prevState.length + 1}`, price: prevState.length * 100 / 34  } ])
   }
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/products')
+      .then((res) => {
+        console.log("Products Get", res.data)
+        setProducts(res.data);
+      })
+      .catch((err) => {
+        console.log("Products Get Error")
+      })
+  }, [])
 
 
   return (
@@ -29,7 +42,7 @@ function App() {
       {
         products.map((product, index) => {
           return(
-            <li key={index}>{product.name}:  ${product.price}</li>
+            <li key={index}>{product.name}:  ${product.price.toFixed(2)}</li>
           )
         })
       }
